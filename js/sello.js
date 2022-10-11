491ca899b21e275412593825f3abc3b3c1ca962d
Enas Sello <94912513+Enas-Sello@users.noreply.github.com>
2022-10-11 10:14:37 +0200

    Add files via upload

---
+++ js/sello.js
@@ -0,0 +1,16 @@
+let span = document.querySelector(".go-top");
+window.onscroll = function(){
+ if (this.scrollY >= 1100){
+     span.classList.add("show")
+  }
+else{
+     span.classList.remove("show")
+  }
+  //this.scrollY >= 1100 ? span.classList.add("show") : span.classList.remove("show")
+};
+
+span.onclick = function() {
+    window.scroll({
+        top:0 ,behavior:"smooth",
+    })
+};
